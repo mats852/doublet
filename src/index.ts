efc421ffaa71f1c5841beec3344e6121927f5ca0
emeraldsanto <yanick.belanger@yahoo.com>
2020-12-31 20:40:01 -0500

    MaybeAsyncResult

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-export default function doublet<Arg, R>(cb: Callback<Arg, R>, ...args: Array<Arg>): Result<R> | Promise<Result<R>> {
+export default function doublet<Arg, R>(cb: Callback<Arg, R>, ...args: Array<Arg>): MaybeAsyncResult<R> {
   try {
     const result = cb(...args);
 
     if (result instanceof Promise) {
       return result
         .then((rx) => [null, rx])
-        .catch((error) => [error, null]) as Promise<Result<R>>;
+        .catch((error) => [error, null]) as MaybeAsyncResult<R>;
     }
 
-    return [null, result];
+    return [null, result] as MaybeAsyncResult<R>;
   } catch (error) {
-    return [error, null];
+    return [error, null] as MaybeAsyncResult<R>;
   }
 }
--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,6 @@
 type Result<R> = [Error, null] | [null, R];
 
 type Callback<Arg, R> = (...args: Array<Arg>) => R;
+
+type MaybeAsyncResult<R> = R extends Promise<infer U> ? Promise<Result<U>> : Result<R>
+
