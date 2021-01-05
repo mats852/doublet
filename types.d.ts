f9c575298380b59986f2fee28ba53cdbd11ea1fa
Yanick Bélanger <yanick.belanger@yahoo.com>
2021-01-05 16:45:27 -0500

    feat: improved type inference for callback parameters (#2)
    * Better types
    
    * Improved doublet argument types
    
    * Removed unnecessary type
    
    * MaybeAsyncResult
    
    * Better function argument types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,5 @@
 type Result<R> = [Error, null] | [null, R];
 
-type Callback<Arg, R> = (...args: Array<Arg>) => R;
+type Callback = (...args: any) => any;
 
 type MaybeAsyncResult<R> = R extends Promise<infer U> ? Promise<Result<U>> : Result<R>
