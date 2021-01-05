9497a62d7a573a2b9b11428808218d64187aefb7
Yanick Bélanger <yanick.belanger@yahoo.com>
2021-01-05 16:34:10 -0500

    Better function argument types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-export default function doublet<Arg, R>(cb: Callback<Arg, R>, ...args: Array<Arg>): MaybeAsyncResult<R> {
+export default function doublet<TCallback extends Callback>(cb: TCallback, ...args: Parameters<TCallback>): MaybeAsyncResult<ReturnType<TCallback>> {
   try {
-    const result = cb(...args);
+    const result = cb(...args as Array<unknown>);
 
     if (result instanceof Promise) {
       return result
         .then((rx) => [null, rx])
-        .catch((error) => [error, null]) as MaybeAsyncResult<R>;
+        .catch((error) => [error, null]) as MaybeAsyncResult<ReturnType<TCallback>>;
     }
 
-    return [null, result] as MaybeAsyncResult<R>;
+    return [null, result] as MaybeAsyncResult<ReturnType<TCallback>>;
   } catch (error) {
-    return [error, null] as MaybeAsyncResult<R>;
+    return [error, null] as MaybeAsyncResult<ReturnType<TCallback>>;
   }
 }
