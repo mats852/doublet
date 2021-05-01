c6ff3286682d0f6e42896cb25c603291ccda68dd
Mathieu St-Vincent <6082025+mats852@users.noreply.github.com>
2021-04-30 22:33:40 -0400

    Feat: Exporting types (#4)

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,11 @@
-type Result<R> = [Error, null] | [null, R];
+/* eslint-disable */
 
 type Callback = (...args: any) => any;
 
-type MaybeAsyncResult<R> = R extends Promise<infer U> ? Promise<Result<U>> : Result<R>
+type Result<R> = [Error, null] | [null, R];
+
+type MaybeAsyncResult<R> = R extends Promise<infer U> ? Promise<Result<U>> : Result<R>;
+
+declare module 'doublet' {
+  export default function doublet<TCallback extends Callback>(cb: TCallback, ...args: Parameters<TCallback>): MaybeAsyncResult<ReturnType<TCallback>>;
+}
